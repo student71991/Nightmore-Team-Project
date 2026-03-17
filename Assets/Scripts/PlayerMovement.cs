d22c5a19f8b109d5ec987526ea967444ac234879
Yoocodehash <lukaszkucharczyk263@gmail.com>
2026-03-17 22:03:13 +0100

    Script and Level Edits

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,84 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    float horizontalInput;
+    float moveSpeed = 5f;
+    bool isFacingRight = false;
+    float jumpPower = 5f;
+    bool isJumping = false;
+
+    private Animator anim;
+
+    Rigidbody2D rb;
+
+    [SerializeField] private AudioSource jumpSoundEffect;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
+        anim = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        FlipSprite();
+
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            isJumping = true;
+            jumpSoundEffect.Play();
+        }
+
+
+
+
+
+        if (horizontalInput > 0f)
+        {
+            anim.SetBool("Running", true);
+        }
+
+        else if (horizontalInput < 0f)
+        {
+            anim.SetBool("Running", true);
+        }
+
+        else
+        {
+            anim.SetBool("Running", false);
+        }
+
+
     }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+    }
+
+    void FlipSprite()
+    {
+        if (isFacingRight && horizontalInput < 0f || !isFacingRight && horizontalInput > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isJumping = false;
+    }
+
+
 }
