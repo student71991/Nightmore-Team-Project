17c44c821bb19364390f637ac9690201b3bd373b
Yoocodehash <lukaszkucharczyk263@gmail.com>
2026-03-20 21:30:11 +0100

    Main And Level Menu Edits

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelSelect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public void Level1()
     {
-        
+        SceneManager.LoadScene("Level1");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Level2()
     {
-        
+        SceneManager.LoadScene("Level2");
     }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+
+    public void GoBackToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("The Player Has Now Quit The Game");
+    }
+
 }
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public void PlayLevel1()
     {
-        
+        SceneManager.LoadScene("Level1");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LevelSelect()
     {
-        
+        SceneManager.LoadScene("LevelSelect");
     }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("The Player Has Now Quit The Game");
+    }
+
 }
