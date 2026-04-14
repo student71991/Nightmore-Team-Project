50a32585a2e67b0eebe3bbd913265e9ee02f5a19
Yoocodehash <lukaszkucharczyk263@gmail.com>
2026-04-14 15:36:46 +0200

    EnemyScriptWIP

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    [SerializeField] private GameObject[] waypoints;
+    private int currentWaypointIndex = 0;
+
+    [SerializeField] private float speed = 2f;
+
+    //private Animator anim;
+
+    /*
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    */
+
+    private void Update()
+    {
+
+        //FlipSprite();
+
+
+        if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < .1f)
+        {
+            currentWaypointIndex++;
+            if (currentWaypointIndex >= waypoints.Length)
+            {
+                currentWaypointIndex = 0;
+            }
+        }
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, Time.deltaTime * speed);
+    }
+
+
+    /*
+    void FlipSprite()
+    {
+        if (isFacingRight && horizontalInput < 0f || !isFacingRight && horizontalInput > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
+        }
+    }
+    */
+
+
+}
